ca1953f76670aa0a5709c9b03545d4fc414f0a6d
jason <jason@tavernier.nl>
2024-12-03 06:30:40 +0100

    solution with regex

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,16 +1,34 @@
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        val regex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+        val fullInput = input.joinToString("")
+        val matches = regex.findAll(fullInput)
+        var answer = 0L
+        matches.forEach {
+            answer+= it.groupValues[1].toLong() * it.groupValues[2].toLong()
+        }
+        return answer
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val fullInput = input.joinToString("")
+        val regexFilter = Regex("don't\\(\\)(.*?)do\\(\\)")
+        val newInput = regexFilter.replace(fullInput, "")
+        val regex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+        newInput.println()
+        val matches = regex.findAll(newInput)
+        var answer = 0L
+        matches.forEach {
+            println(it.groupValues)
+            answer+= it.groupValues[1].toLong() * it.groupValues[2].toLong()
+        }
+        return answer
     }
 
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 1)
-    // check(part2(testInput) == 1)
+    check(part1(testInput) == 161L)
+    check(part2(testInput) == 48L)
     
     val input = readInput("Day03")
     part1(input).println()
