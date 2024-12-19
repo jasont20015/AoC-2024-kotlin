c446bef457eea9dc5f4a16baef02d4bcb01f20b4
jason <jason@tavernier.nl>
2024-12-19 06:48:14 +0100

    day 18 & day 19

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,16 +1,75 @@
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun maze(points: List<coordinate>, endLocation: coordinate): Int?{
+        val start = (0 to 0)
+        val end = endLocation
+
+        val seen = mutableSetOf<coordinate>()
+        val toVisit = mutableListOf(Pair(start, 0))
+
+        while(toVisit.isNotEmpty()) {
+            val (currentPoint, currentDistance) = toVisit.removeAt(0)
+            if(currentPoint in seen){
+                continue
+            }
+            if(currentPoint == end){
+                return currentDistance
+            }
+            val directions = listOf(1 to 0, -1 to 0, 0 to 1, 0 to -1)
+            for((dx,dy) in directions){
+                val newPoint = coordinate(currentPoint.first + dx, currentPoint.second + dy)
+                if(newPoint.first in 0..end.first && newPoint.second in 0..end.second && newPoint !in seen && newPoint !in points)
+                    toVisit.add(Pair(newPoint, currentDistance+1))
+            }
+            seen.add(currentPoint)
+        }
+
+        return null
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>, test: Boolean=false): Int {
+        var size = coordinate(70,70)
+        var simulationSize = 1024
+        val corruptedCoordinates = mutableListOf<coordinate>()
+        if(test){
+            size = coordinate(6,6)
+            simulationSize = 12
+        }
+        for(i in 0..<simulationSize){
+            val coords = input[i].split(",").map{it.toInt()}
+            corruptedCoordinates.add(coords.first() to coords.last())
+        }
+        val answer = maze(corruptedCoordinates, size)
+        if(answer != null)
+            return answer
+        return -1
+    }
+
+    fun part2(input: List<String>, test:Boolean=false): String {
+        var size = coordinate(70,70)
+        var simulationSize = 1024
+        val corruptedCoordinates = mutableListOf<coordinate>()
+        if(test){
+            size = coordinate(6,6)
+            simulationSize = 12
+        }
+        for(i in 0..<input.size){
+            val coords = input[i].split(",").map{it.toInt()}
+            corruptedCoordinates.add(coords.first() to coords.last())
+        }
+        for(i in 0..<corruptedCoordinates.size){
+            if(maze(corruptedCoordinates.subList(0, i), size) == -1){
+                val coordinates = corruptedCoordinates.get(i)
+                return "${coordinates.first},${coordinates.second}"
+            }
+        }
+        return "maze always reachable"
     }
 
     val testInput = readInput("Day18_test")
-    check(part1(testInput) == 1)
-    // check(part2(testInput) == 1)
+    check(part1(testInput, true) == 22)
+    println(part2(testInput, true))
+    check(part2(testInput, true) == "6,1")
     
     val input = readInput("Day18")
     part1(input).println()
--- src/Day19.kt
+++ src/Day19.kt
@@ -1,16 +1,38 @@
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val towelsTried = HashMap<String, Long>()
+    fun towelsFit(towels: List<String>, output: String): Long{
+        if (output.isEmpty()) return 1L
+        towelsTried[output]?.let { return it }
+        return towels.sumOf {
+            if (output.startsWith(it))
+                towelsFit (towels, output.drop(it.length))
+            else
+                0
+        }.also { towelsTried[output] = it }
+
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Int {
+        val towels = input[0].split(",").map{it.trim()}
+        val outputs = input.drop(2)
+
+        towelsTried.clear()
+
+        return outputs.count { towelsFit(towels, it) >= 1 }
+    }
+
+    fun part2(input: List<String>): Long {
+        val towels = input[0].split(",").map{it.trim()}
+        val outputs = input.drop(2)
+
+        return outputs.sumOf { towelsFit(towels, it) }
     }
 
     val testInput = readInput("Day19_test")
-    check(part1(testInput) == 1)
-    // check(part2(testInput) == 1)
+    println(part1(testInput))
+    check(part1(testInput) == 6)
+    check(part2(testInput) == 16L)
     
     val input = readInput("Day19")
     part1(input).println()
