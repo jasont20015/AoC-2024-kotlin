18683996738b2105f8cf69890cd31652019999a1
jason <jason@tavernier.nl>
2024-12-25 06:49:34 +0100

    day25

--- src/Day25.kt
+++ src/Day25.kt
@@ -1,6 +1,61 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val locks = mutableListOf<List<String>>()
+        val keys = mutableListOf<List<String>>()
+        val locksSizes = mutableListOf<List<Int>>()
+        val keysSizes = mutableListOf<List<Int>>()
+        val holeSize = 5
+        val inputSeperated = input.joinToString("\n").split("\n\n")
+
+        inputSeperated.forEach {
+            if(it.startsWith("#####")){
+                locks.add(it.split("\n"))
+            } else{
+                keys.add(it.split("\n"))
+            }
+        }
+        locks.forEach {
+            val currentLock = mutableListOf<Int>()
+            for(x in 0..<it[0].length) {
+                for (y in 0..<it.size) {
+                    if (it[y][x] == '.') {
+                        currentLock.add(y-1)
+                        break
+                    }
+                }
+            }
+            locksSizes.add(currentLock)
+        }
+        keys.forEach {
+            val currentKey = mutableListOf<Int>()
+            for(x in 0..<it[0].length) {
+                for (y in 0..<it.size) {
+                    if (it[y][x] == '#') {
+                        currentKey.add(holeSize-(y-1))
+                        break
+                    }
+                }
+            }
+            keysSizes.add(currentKey)
+        }
+        var answer = 0
+        locksSizes.forEach { l ->
+            keysSizes.forEach { k ->
+                var keyFits = true
+                for(i in 0..<k.size){
+                    if(holeSize-k[i]-l[i] < 0){
+                        keyFits = false
+                    }
+
+                }
+                if(keyFits){
+                    answer += 1
+                }
+            }
+        }
+
+
+        return answer
     }
 
     fun part2(input: List<String>): Int {
@@ -8,7 +63,7 @@ fun main() {
     }
 
     val testInput = readInput("Day25_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
     // check(part2(testInput) == 1)
 
     val input = readInput("Day25")
